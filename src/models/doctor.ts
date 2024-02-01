197cc30d877890dc7c18c600a68d8850aad5a92b
vanis <pavlik.vanis@email.cz>
2024-02-01 19:17:04 +0100

    Create model for Doctor

---
+++ src/models/doctor.ts
@@ -0,0 +1,69 @@
+import mongoose, { Document, Types, Schema, Model } from "mongoose";
+import MedicalWorkspace from "./medical_workspace";
+import DepartmentModel from "./department";
+
+export interface IDoctor extends Document {
+  first_name: string;
+  last_name: string;
+  phone?: string;
+  email?: string;
+  identification_number: string;
+  medical_workspace?: Types.ObjectId;
+  department?: Types.ObjectId;
+}
+
+interface IMethods {}
+
+const DoctorSchema = new Schema<IDoctor, {}, IMethods>(
+  {
+    first_name: {
+      type: String,
+      required: [true, "First name is required"],
+      minlength: [1, "First name must be at least 1 characters long"],
+      maxlength: [36, "First name must be at most 236 characters long"],
+    },
+    last_name: {
+      type: String,
+      required: [true, "Last name is required"],
+      minlength: [1, "Last name must be at least 1 characters long"],
+      maxlength: [36, "Last name must be at most 236 characters long"],
+    },
+    phone: {
+      type: String,
+      unique: true,
+      required: false,
+      match: [
+        /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/,
+        "Invalid phone number",
+      ],
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: false,
+      match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, "Invalid email"],
+    },
+    // Should be validateed with some pattern
+    identification_number: {
+      type: String,
+      unique: true,
+      required: [true, "Identification number is required"],
+    },
+    medical_workspace: {
+      type: Schema.Types.ObjectId,
+      ref: MedicalWorkspace,
+      required: false,
+    },
+    department: {
+      type: Schema.Types.ObjectId,
+      ref: DepartmentModel,
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const DoctorModel =
+  mongoose.models.doctor || mongoose.model("doctor", DoctorSchema);
+
+export default DoctorModel as Model<IDoctor, {}, IMethods>;
