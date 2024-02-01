b5ab523de124858ff5c007aea1b3264b75ac3fa9
vanis <pavlik.vanis@email.cz>
2024-02-01 19:16:33 +0100

    Page updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-xl font-semibold">Database system app</h1>
+      <h1 className="text-xl font-semibold">Database system app for <span className=" text-[1.3rem]">Alfa 3</span></h1>
     </main>
   );
 }
