1396ca7ac70fbe7ef1a1c4a6c38c2b398b57b8b2
vanis <pavlik.vanis@email.cz>
2024-02-03 16:42:21 +0100

    Create REST API for patients

---
+++ src/app/api/patients/route.ts
@@ -0,0 +1,52 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import PatientModel, { IPatient, IPatientSchema } from "@/models/patient";
+import { SuccessResponse } from "@/types/api-request";
+import { errorHandler } from "@/utils/error-handler";
+import zodValidate from "@/utils/zod-validate";
+
+// Get all patients
+export const GET = async () => {
+  try {
+    const patients = await PatientModel.find({});
+
+    return NextResponse.json<SuccessResponse<IPatient[]>>(
+      { items: patients },
+      { status: 200 }
+    );
+  } catch (error) {
+    return errorHandler(error);
+  }
+};
+
+// Create new patient
+export const POST = async (req: NextRequest) => {
+  try {
+    const body = (await req.json()) as IPatient;
+
+    // Add mongo sanitization
+
+    // Zod validation
+    const validatedBody = zodValidate(IPatientSchema, {
+      ...body,
+      birth_date: new Date(body.birth_date),
+    });
+
+    // Check if patient already exists
+    const patientExist = await PatientModel.findOne({
+      born_id: validatedBody.born_id,
+    });
+    if (patientExist)
+      return new NextResponse("Patient already exists!", { status: 400 });
+
+    // Create patient
+    const patient = await PatientModel.create(validatedBody);
+
+    return NextResponse.json<SuccessResponse<IPatient>>(
+      { items: patient },
+      { status: 200 }
+    );
+  } catch (error) {
+    return errorHandler(error);
+  }
+};
