7abd19962cc16cb95d10c076986f94f11df6b759
vanis <pavlik.vanis@email.cz>
2024-02-03 16:44:41 +0100

    Update imports from models

--- src/models/index.js
+++ src/models/index.js
@@ -3,7 +3,7 @@ export { default as DoctorModel, IDoctor } from "./doctor";
 export {
   default as MedicalWorkspaceModel,
   IMedicalWorkspace,
-} from "./medical_workspace";
+} from "./medical-workspace";
 export { default as MedicineModel, IMedicine } from "./medicine";
 export { default as PatientModel, IPatient } from "./patient";
 export { default as PrescriptionModel, IPrescription } from "./prescription";
