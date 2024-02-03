905a62a7c2a3cdd1f023ed559ed99dd21380a498
vanis <pavlik.vanis@email.cz>
2024-02-03 11:10:11 +0100

    export models

---
+++ src/models/index.js
@@ -0,0 +1,9 @@
+export { default as DepartmentModel, IDepartment } from "./department";
+export { default as DoctorModel, IDoctor } from "./doctor";
+export {
+  default as MedicalWorkspaceModel,
+  IMedicalWorkspace,
+} from "./medical_workspace";
+export { default as MedicineModel, IMedicine } from "./medicine";
+export { default as PatientModel, IPatient } from "./patient";
+export { default as PrescriptionModel, IPrescription } from "./prescription";
