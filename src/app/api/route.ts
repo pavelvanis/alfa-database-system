270e61110199f5b6de77e424e8e09f75122c3782
vanis <pavlik.vanis@email.cz>
2024-02-03 11:05:31 +0100

    Create routes for rest API

---
+++ src/app/api/route.ts
@@ -0,0 +1,19 @@
+import { SuccessResponse, ErrorResponse } from "@/types/api-request";
+import mongoose from "mongoose";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (req: NextRequest, res: NextResponse) => {
+  try {
+    const state =
+      mongoose.connection.readyState == 1 ? "connected" : "disconnected";
+
+    return NextResponse.json(
+      { message: state },
+      { status: 200 }
+    ) as SuccessResponse<string>;
+  } catch (error) {
+    return new NextResponse("Something went wrong", {
+      status: 500,
+    }) as ErrorResponse<string>;
+  }
+};
