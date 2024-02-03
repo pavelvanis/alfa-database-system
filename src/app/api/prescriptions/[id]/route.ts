369aa7fcb6a8461a12f0222b1fcf292684a35937
vanis <pavlik.vanis@email.cz>
2024-02-03 19:00:28 +0100

    Create REST API for prescription

---
+++ src/app/api/prescriptions/[id]/route.ts
@@ -0,0 +1,87 @@
+import { isValidObjectId } from "mongoose";
+import { NextRequest, NextResponse } from "next/server";
+
+import { SuccessResponse } from "@/types/api-request";
+import { errorHandler } from "@/utils/error-handler";
+import zodValidate from "@/utils/zod-validate";
+import { IPrescription, PrescriptionModel } from "@/models";
+import { PrescriptionSchemaZodOpt } from "@/models/prescription";
+
+// Get prescription by id
+export const GET = async (
+  req: NextRequest,
+  { params: { id } }: { params: { id: string } }
+) => {
+  try {
+    // Check valid id
+    if (!isValidObjectId(id))
+      return errorHandler("Invalid prescription id", 400);
+
+    // Find prescription by id
+    const prescription = await PrescriptionModel.findById(id);
+    if (!prescription) return errorHandler("Prescription not found", 404);
+
+    return NextResponse.json<SuccessResponse<IPrescription>>({
+      items: prescription,
+    });
+  } catch (error) {
+    return errorHandler(error);
+  }
+};
+
+// Update prescription by id
+export const PUT = async (
+  req: NextRequest,
+  { params: { id } }: { params: { id: string } }
+) => {
+  try {
+    const body = (await req.json()) as IPrescription;
+
+    // Zod validation
+    const validatedBody = zodValidate(PrescriptionSchemaZodOpt, body);
+
+    // Check valid id
+    if (!isValidObjectId(id))
+      return errorHandler("Invalid prescription id", 400);
+
+    // Update prescription by id
+    const updatedPrescription = await PrescriptionModel.findByIdAndUpdate(
+      id,
+      validatedBody,
+      {
+        new: true,
+      }
+    );
+
+    if (!updatedPrescription)
+      return errorHandler("Prescription not found", 404);
+    return NextResponse.json<SuccessResponse<IPrescription>>({
+      items: updatedPrescription,
+    });
+  } catch (error) {
+    return errorHandler(error);
+  }
+};
+
+// Delete prescription by id
+export const DELETE = async (
+  req: NextRequest,
+  { params: { id } }: { params: { id: string } }
+) => {
+  try {
+    // Check valid id
+    if (!isValidObjectId(id))
+      return errorHandler("Invalid prescription id", 400);
+
+    // Delete prescription by id
+    const deletedPrescription = await PrescriptionModel.findByIdAndDelete(id);
+
+    if (!deletedPrescription)
+      return errorHandler("Prescription not found", 404);
+    return NextResponse.json<SuccessResponse<IPrescription>>({
+      items: deletedPrescription,
+    });
+  } catch (error) {
+    return errorHandler(error);
+  }
+};
---
+++ src/app/api/prescriptions/route.ts
@@ -0,0 +1,69 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { SuccessResponse } from "@/types/api-request";
+import { errorHandler } from "@/utils/error-handler";
+import zodValidate from "@/utils/zod-validate";
+import {
+  DoctorModel,
+  IPrescription,
+  MedicineModel,
+  PatientModel,
+  PrescriptionModel,
+} from "@/models";
+import { PrescriptionSchemaZod } from "@/models/prescription";
+
+// Get all prescriptions
+export const GET = async () => {
+  try {
+    const prescriptions = await PrescriptionModel.find({});
+
+    return NextResponse.json<SuccessResponse<IPrescription[]>>(
+      { items: prescriptions },
+      { status: 200 }
+    );
+  } catch (error) {
+    return errorHandler(error);
+  }
+};
+
+// Create new prescription
+export const POST = async (req: NextRequest) => {
+  try {
+    const body = (await req.json()) as IPrescription;
+
+    // Add mongo sanitization
+
+    // Zod validation
+    const validatedBody: IPrescription = zodValidate(PrescriptionSchemaZod, {
+      ...body,
+      expires_at: new Date(body.expires_at),
+      picked_at: body.picked_at ? new Date(body.picked_at) : undefined,
+    });
+
+    // Check if prescription already exists
+    const prescriptionExist = await PrescriptionModel.findOne({});
+    if (prescriptionExist)
+      return new NextResponse("Prescription already exists!", { status: 400 });
+
+    const doctorExist = await DoctorModel.findById(validatedBody.doctor);
+    if (!doctorExist) return errorHandler("Doctor not found!", 404);
+
+    const patientExist = await PatientModel.findById(validatedBody.patient);
+    if (!patientExist) return errorHandler("Patient not found!", 404);
+
+    // Check if medicines exist
+    validatedBody.medicines.forEach(async (medicine) => {
+      const medicineExist = await MedicineModel.findById(medicine.medicine_id);
+      if (!medicineExist) return errorHandler("Medicine not found!", 404);
+    });
+
+    // Create prescription
+    const prescription = await PrescriptionModel.create(validatedBody);
+
+    return NextResponse.json<SuccessResponse<IPrescription>>({
+      items: prescription,
+    });
+  } catch (error) {
+    return errorHandler(error);
+  }
+};
